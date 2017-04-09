9599189db1f9d2647d11ef3ef1a609121f5cca66
code3lue <alan.delez@gmail.com>
2017-04-09 16:50:59 -0400

    stuck on basic

---
+++ basicFlash.js
@@ -0,0 +1,18 @@
+//var fs = require("fs");
+
+var BasicCard = function(front, back) {
+    this.front = front;
+    this.back = back; 
+
+
+
+var firstPresident = new BasicCard(
+    "Who was the fist president of the United States?", "George Washington"
+);
+
+};
+
+
+
+
+module.exports = BasicCard;
